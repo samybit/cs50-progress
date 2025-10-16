e0f3d4d390f616b11da53628a872e72ec2e329aa
samysamir-dev <samyb.samir@gmail.com>
2025-10-16 06:38:47 +0300

    Add week 3

--- Week 1 C/cash/cash.c
+++ Week 1 C/cash/cash.c
@@ -1,7 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
-// function pattern
+// Function prototypes
 int calculate_quarters(int cents);
 int calculate_dimes(int cents);
 int calculate_nickels(int cents);
